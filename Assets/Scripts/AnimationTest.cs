6b87e65f2ce6a16a2cdf4189b01ac97a7a6e79e6
Unknown <patrickmonville@hotmail.com>
2017-01-09 16:24:25 +0100

    the real initial commit

---
+++ Assets/Scripts/AnimationTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationTest : MonoBehaviour
+{
+    private Animator _animatorComponent;
+
+    // Use this for initialization
+    void Start()
+    {
+        _animatorComponent = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _animatorComponent.SetTrigger("DeathTrigger");
+        }
+    }
+}
---
+++ Assets/Scripts/GuidedProjectileController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuidedProjectileController : ProjectileController
+{
+    public float _radarRange = 5;
+
+    protected new void MoveProjectile()
+    {
+        RaycastHit[] raycastHit = Physics.SphereCastAll(this.transform.position, _radarRange, Vector3.zero);
+
+        if (raycastHit != null && raycastHit.Length > 0)
+        {
+
+        }
+        else
+        {
+            base.MoveProjectile();
+        }
+    }
+}
